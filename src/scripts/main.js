37728c0fda800d3196345fc199c1e263606f8736
Glib Rulev <velzepooz@gmail.com>
2020-06-15 09:57:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,56 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  const daysOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
+  element.innerHTML = `
+    <table>
+      <thead>
+        <tr>
+          ${daysOfWeek.map(day => `
+          <th>${day}</th>
+          `).join('')}
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+  `;
+
+  const date = new Date(year, month - 1);
+  const daysInMonth = new Date(year, month, 0).getDate();
+
+  let startDayNumber = date.getDay();
+
+  if (startDayNumber === 0) {
+    startDayNumber = 7;
+  }
+
+  const calendarDates = [...Array(daysInMonth + 1).keys()];
+
+  calendarDates.splice(0, 1);
+
+  const tbody = document.getElementsByTagName('tbody')[0];
+
+  let counter = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const week = tbody.insertRow();
+
+    for (let j = 0; j < 7; j++) {
+      const day = week.insertCell();
+
+      if (counter !== calendarDates.length) {
+        if (tbody.childElementCount === 1) {
+          if (week.childElementCount >= startDayNumber) {
+            day.innerHTML = `${calendarDates[counter++]}`;
+          }
+        } else {
+          day.innerHTML = `${calendarDates[counter++]}`;
+        }
+      }
+    }
+  }
 }
 
 calendarTable(2019, 10, calendar);
